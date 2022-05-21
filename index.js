176fc9380297ab56412012af3b8777203b1439b7
Reynaldi Chernando <reynaldichernando@gmail.com>
2022-05-21 16:54:37 +0700

    Cleanup code

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const c = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
 
+class Table {
+    constructor() { }
+
+    draw() {
+        c.fillStyle = 'black';
+        c.fillRect(0, 0, canvas.width, canvas.height);
+
+        c.strokeStyle = 'white';
+        c.setLineDash([10, 30]);
+        c.lineWidth = 3;
+        c.beginPath();
+        c.moveTo(canvas.width / 2, 0);
+        c.lineTo(canvas.width / 2, canvas.height);
+        c.closePath();
+        c.stroke();
+    }
+}
+
 class Ball {
     constructor(x, y, radius, color, velocity) {
         this.x = x;
@@ -20,14 +38,6 @@ class Ball {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
-
-        // c.strokeStyle = 'red';
-        // c.beginPath();
-        // c.setLineDash([0]);
-        // c.moveTo(0, this.y);
-        // c.lineTo(canvas.width, this.y);
-        // c.closePath();
-        // c.stroke();
     }
 
     update() {
@@ -83,83 +93,90 @@ class Opponent extends Player {
         super(x, y, w, h, color);
         this.difficulty = difficulty;
     }
+
+    update() {
+        this.draw();
+    }
+
+    move(ball) {
+        if (counter < this.difficulty
+            && ball.x > canvas.width / 2
+            && ball.x > ball.prevX
+            && ball.x < canvas.width
+        ) {
+            if (ball.y <= this.y) {
+                this.up();
+            }
+            else if (ball.y >= this.y + this.h) {
+                this.down();
+            }
+        }
+    }
 }
 
-let random = Math.random();
+let random;
+let angle;
+let modifier;
+let counter;
 
-while ((0.15 < random && random < 0.35) || (0.65 < random && random < 0.85) || random == 0.5 || random == 0) {
-    random = Math.random();
-}
-
-let angle = random * Math.PI * 2;
-
-let modifier = 5;
-
-let ball = new Ball(canvas.width / 2, canvas.height / 2, 8, 'white', {
-    x: Math.cos(angle) * modifier,
-    y: Math.sin(angle) * modifier
-});
-
-let paddleWidth = 4;
-let paddleHeight = 100;
-let player = new Player(
-    10,
-    canvas.height / 2 - paddleHeight / 2,
-    paddleWidth,
-    paddleHeight,
-    'white'
-);
-let opponent = new Opponent(
-    canvas.width - 10 - paddleWidth,
-    canvas.height / 2 - paddleHeight / 2,
-    paddleWidth,
-    paddleHeight,
-    'white',
-    50
-);
-
-let counter = 0;
+let paddleWidth;
+let paddleHeight;
 
 let animationId;
+
+let ball;
+let player;
+let opponent;
+let table;
+
+function init() {
+    random = 0;
+    while ((0.15 < random && random < 0.35)
+        || (0.65 < random && random < 0.85)
+        || random == 0.5
+        || random == 0
+    ) random = Math.random();
+
+    angle = random * Math.PI * 2;
+    modifier = 5;
+    counter = 0;
+
+    paddleWidth = 4;
+    paddleHeight = 100;
+
+    ball = new Ball(canvas.width / 2, canvas.height / 2, 8, 'white', {
+        x: Math.cos(angle) * modifier,
+        y: Math.sin(angle) * modifier
+    });
+    player = new Player(
+        10,
+        canvas.height / 2 - paddleHeight / 2,
+        paddleWidth,
+        paddleHeight,
+        'white'
+    );
+    opponent = new Opponent(
+        canvas.width - 10 - paddleWidth,
+        canvas.height / 2 - paddleHeight / 2,
+        paddleWidth,
+        paddleHeight,
+        'white',
+        50
+    );
+    table = new Table();
+}
+
 function animate() {
     animationId = requestAnimationFrame(animate);
+
     counter += 1;
-
-    // table
-    c.fillStyle = 'black';
-    c.fillRect(0, 0, canvas.width, canvas.height);
-
-    // line separator
-    c.strokeStyle = 'white';
-    c.setLineDash([10, 30]);
-    c.lineWidth = 3;
-    c.beginPath();
-    c.moveTo(canvas.width / 2, 0);
-    c.lineTo(canvas.width / 2, canvas.height);
-    c.closePath();
-    c.stroke();
+    if (counter == 60) counter = 0;
 
     // player action
     if (keys["ArrowUp"]) player.up();
     if (keys["ArrowDown"]) player.down();
 
-    if (counter == 60) {
-        counter = 0;
-    }
-
-    // opponent logic
-    if (counter < opponent.difficulty
-        && ball.x > canvas.width / 2
-        && ball.x > ball.prevX
-        && ball.x < canvas.width
-    ) {
-        if (ball.y <= opponent.y) {
-            opponent.up();
-        }
-        else if (ball.y >= opponent.y + opponent.h) {
-            opponent.down();
-        }
-    }
+    opponent.move(ball);
 
     // collision logic
     if (player.x + player.w > ball.x - ball.radius
@@ -186,6 +203,7 @@ function animate() {
         ball.velocity.x *= -1.05;
     }
 
+    table.draw();
     opponent.update();
     player.update();
     ball.update();
@@ -201,4 +219,5 @@ window.addEventListener('keyup', (event) => {
     delete keys[event.key];
 });
 
+init();
 animate();
